565a01bd5f176868d937ca6f70886c7bcc78a4ce
David Tang <dtang859@gmail.com>
2014-11-06 00:34:29 -0800

    update

--- js/main.js
+++ js/main.js
@@ -8,22 +8,24 @@ app.factory('Book', function(DS) {
   });
 });
 
-app.controller('BooksController', function(DS, Book) {
-	var vm = this;
+app.controller('BooksController', function($scope, DS) {
+  DS.bindAll($scope, 'books', 'book')
 
-  vm.title = "My Books";
-
-  DS.findAll('book').then(function(books) {
-  	console.log(books);
-    vm.books = books;
-
-    // no request made here since this book is already in the store
-    DS.find('book', '978-0596806750').then(function(book) {
-    	console.log(book, 'loaded directly from data store');
-    });
+  // fetch books from endpoint and load into the store
+  DS.findAll('book').then(function() {
+  	// no request made here since this book is already in the store
+  	DS.find('book', '978-0596806750').then(function(book) {
+	  	console.log(book, 'loaded directly from data store');
+	  });
   });
 });
 
-// app.run(function(Book) {
-
-// });
+app.run(function(Book) {
+	// This could be from a server side data dump
+	Book.inject([
+		{
+			"isbn13": "111-2222222222",
+			"title": "Test Book"
+		}
+	]);
+});
